2be86a39ed1513f69c5d8e5bd21dcc2a080ff88d
Andrew Attard <and_att@hotmail.com>
2016-05-26 21:26:43 +0300

    commit 5

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-"# las3009_assignment" 
-"# las3009_assignment" 
+1. download
+
+git clone https://github.com/andrewatt/las3009_assignment.git
+
+2. installation
+
+cd ...\las3009_assignment
+
+npm install grunt
+
+npm install grunt-contrib-watch
+
+npm install grunt-contrib-connect
+
+bower install angular
+
+bower install angular-chart.js --save
+
+bower install --save angular-cookies
+
+bower install angular-route
+
+3. run
+
+json-server --watch db.json
+
+grunt serve
+
+4. url
+
+http://localhost:1234/#/
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -27,25 +27,22 @@
                 });
 
             $scope.getBalance = function () {
-                /*
-                 var income = 0;
-                 this.transactions.forEach(function (transaction) {
-                 if (transaction.type === 'income') {
-                 income += (1 * transaction.price);
-                 }
-                 })
-                 var expense = 0;
-                 this.transactions.forEach(function (transaction) {
-                 if (transaction.type === 'expense') {
-                 expense += (1 * transaction.price);
-                 }
-                 })
-                 this.prices = [income, expense];
-                 var balance = income - expense;
-                 balance = Math.round(balance * 100) / 100;
-                 return balance;
-                 */
-                return 5;
+                var income = 0;
+                this.transactions.forEach(function (transaction) {
+                    if (transaction.type === 'income') {
+                        income += (1 * transaction.price);
+                    }
+                })
+                var expense = 0;
+                this.transactions.forEach(function (transaction) {
+                    if (transaction.type === 'expense') {
+                        expense += (1 * transaction.price);
+                    }
+                })
+                this.prices = [income, expense];
+                var balance = income - expense;
+                balance = Math.round(balance * 100) / 100;
+                return balance;
             }
 
         }]);
